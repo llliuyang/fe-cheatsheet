1a1fbdb380cd8c96b8e610b9de56ec2997b12a5d
llliuyang <1714610279@qq.com>
2024-01-14 16:00:01 +0800

    新增文章：优先级与权重计算

--- docs/.vitepress/config/sidebarConfig.ts
+++ docs/.vitepress/config/sidebarConfig.ts
@@ -5,7 +5,8 @@ const SidebarConfig = {
       items: [
         { text: '引言', link: '/css/' },
         { text: '选择器明晰', link: '/css/selector' },
-        { text: '选择器明晰（续）', link: '/css/selector-2' }
+        { text: '选择器明晰（续）', link: '/css/selector-2' },
+        { text: '优先级与权重计算', link: '/css/specificity' }
       ]
     }
   ]
